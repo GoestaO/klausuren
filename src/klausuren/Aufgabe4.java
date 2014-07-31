45230c1ea9a873cff7dcc8c4e60cd5c2a12e16d8
goesta <goesta@goesta-VirtualBox>
2014-07-31 13:53:03 +0200

    init

---
+++ src/klausuren/Aufgabe4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klausuren;
+
+/**
+ *
+ * @author Gösta Ostendorf <goesta.o@gmail.com>
+ */
+public class Aufgabe4 {
+
+    public double berechneWurzel(int n, int a) {
+        double zwischenwert = 0;
+        if (n == 0) {
+            zwischenwert = a;
+        }
+        if (n > 0 && a > 0) {
+            zwischenwert = (berechneWurzel(n - 1, a) + a / berechneWurzel(n - 1, a)) / 2;
+        }
+        return zwischenwert;
+    }
+
+    public int[] berechneDreieckszahlen(int n) {
+        if (n < 0) {
+            return null;
+        }
+        int[] array = new int[n];
+        array[0] = 0;
+        for (int i = 1; i < n; i++) {
+            array[i] = array[i - 1] + i;
+        }
+        return array;
+    }
+}
---
+++ src/klausuren/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package klausuren;
+
+/**
+ *
+ * @author Gösta Ostendorf <goesta.o@gmail.com>
+ */
+public class Main {
+    public static void main(String[] args) {
+        Aufgabe4 q = new Aufgabe4();
+//        System.out.println("q = " + q.berechneWurzel(6, 2));
+//        System.out.println("q = " + q.berechneDreieckszahlen(4));
+        int[]x = q.berechneDreieckszahlen(6);
+        for(int i : x){
+            System.out.println("i = " + i);
+        }
+    }
+    
+    
+    
+}
